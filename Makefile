65e571658fe6480e2f734eb51766be89f6c81b81
LcsH0s <lucasdecastro.web@gmail.com>
2022-08-26 18:06:39 +0200

    README update

--- Makefile
+++ Makefile
@@ -1,10 +1,7 @@
-all: setup run
+all: setup
 
 setup:
 	brew install graphviz
 	export GRAPHVIZ_DIR="/usr/local/Cellar/graphviz/5.0.1"
 	python -m pip install pygraphviz --global-option=build_ext --global-option="-I$GRAPHVIZ_DIR/include" --global-option="-L$GRAPHVIZ_DIR/lib"
 	python -m pip install -r ./requirements.txt
-
-run:
-	python src/app.py
--- src/usage_example.py
+++ src/usage_example.py
@@ -4,6 +4,9 @@ from tacap_lib import TACapClient
 def main():
     tw = TACapClient(eco_mode=True)
     tw.draw_social_graph('Ilias__ll', 'both', filename='ilias.pdf')
+    tw.save_graph_dict('json')
+    # tw.load_graph_dict('json', '/path/to/previously_saved_graph_in_json_format.json')
+    # tw.load_graph_dict('pickle', '/path/to/previously_saved_graph_in_pickle_format.pickle')
 
 
 if __name__ == '__main__':
