7421a991563f6698a7db10229be8008915f1d69b
LcsH0s <lucasdecastro.web@gmail.com>
2022-08-26 18:15:03 +0200

    'better' ue

--- usage_example.py
+++ usage_example.py
@@ -3,8 +3,12 @@ from src.tacap_lib import TACapClient
 
 def main():
     tw = TACapClient(eco_mode=True)
-    tw.draw_social_graph('Ilias__ll', 'both', filename='ilias.pdf')
-    tw.save_graph_dict('json')
+
+    tw.draw_social_graph(screen_name='Ilias__ll',
+                         mode='both', filename='ilias.pdf')
+
+    tw.save_graph_dict(format='json')
+
     # tw.load_graph_dict('json', '/path/to/previously_saved_graph_in_json_format.json')
     # tw.load_graph_dict('pickle', '/path/to/previously_saved_graph_in_pickle_format.pickle')
 
